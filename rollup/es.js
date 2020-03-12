7eaf4bd53e2340c26ffe89bba7a2169937747965
Paulo Coghi <paulocoghi@gmail.com>
2020-03-12 12:44:31 -0300

    Updated devDependencies

--- rollup/es.js
+++ rollup/es.js
@@ -5,7 +5,7 @@ import { terser } 		from 'rollup-plugin-terser';
 export default {
 	input: 'src/index.js',
 
-	external: [ 'crypto', 'events', 'fs', 'http', 'https', 'net', 'os', 'path', 'querystring', 'stream', 'tls', 'url', 'util', 'zlib' ],
+	external: [ 'bufferutil', 'crypto', 'events', 'fs', 'http', 'https', 'net', 'os', 'path', 'querystring', 'stream', 'tls', 'url', 'utf-8-validate', 'util', 'zlib' ],
 
 	output: {
 		file: 'dist/liven.es.js',
